4a7426bb5d94c7bf4e8e6a21d358c924ec08060f
davudxD <carovacdavud6@gmail.com>
2025-07-01 18:18:30 +0200

    deploy corrections

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,9 +2,8 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
-import { Menu } from "primereact/menu";
 import { Button } from "primereact/button";
 import "primeicons/primeicons.css";
 import frostyImg from "../assets/frostyImg-transparent.png";
