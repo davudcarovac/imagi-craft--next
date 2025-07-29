f8e4cdf3a3b6b5fc876e8f957c58f3b13c58a545
davudxD <carovacdavud6@gmail.com>
2025-07-29 13:07:17 +0200

    back to previous config

--- server/src/middlewares/csrfProtection.ts
+++ server/src/middlewares/csrfProtection.ts
@@ -19,11 +19,8 @@ export const csrfProtection = (
     const token = generateCsrfToken();
     console.log("Get token ===> ", token);
     res.cookie("XSRF-TOKEN", token, {
-      // secure: process.env.NODE_ENV === "production",
-      secure: false,
-
-      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       httpOnly: false,
       path: "/",
       maxAge: 48 * 60 * 60 * 1000,
