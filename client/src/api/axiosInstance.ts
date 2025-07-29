c849d63fa5bffd153924b11f5cf62614be6572c5
davudxD <carovacdavud6@gmail.com>
2025-07-29 08:24:12 +0200

    frontend and backend corrections

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const BASE_URL =
-//   process.env.NODE_ENV === "production"
-//     ? process.env.NEXT_PUBLIC_BASE_URL
-//     : "http://localhost:4000";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_BASE_URL
+    : "http://localhost:4000";
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: BASE_URL,
   // withCredentials: true, // 👈 Obavezno za slanje cookie-a
 });
 
