bbf2e67da80dafb0d333d7d6eaf81bb4338598f1
davudxD <carovacdavud6@gmail.com>
2025-07-29 07:36:37 +0200

    frontend correction

--- client/src/api/axiosInstance.ts
+++ client/src/api/axiosInstance.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_BASE_URL
-    : "http://localhost:4000";
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? process.env.NEXT_PUBLIC_BASE_URL
+//     : "http://localhost:4000";
 export const axiosInstance = axios.create({
   baseURL: "https://frosty-image-server.onrender.com",
 });
