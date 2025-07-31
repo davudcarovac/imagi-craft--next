d92fb9adc238f4fd63b73fb3d3af3b56323b3381
davudxD <carovacdavud6@gmail.com>
2025-07-31 19:17:40 +0200

    allowed headers update

--- server/dist/app.js
+++ server/dist/app.js
@@ -34,7 +34,7 @@ app.use(cors({
     origin: allowedOrigins,
     credentials: true, // Ovo je ključno za cookies
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
 }));
 // app.options("*", cors());
 app.use(cookieParser());
--- server/src/app.ts
+++ server/src/app.ts
@@ -42,7 +42,7 @@ app.use(
     origin: allowedOrigins,
     credentials: true, // Ovo je ključno za cookies
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
   })
 );
 
