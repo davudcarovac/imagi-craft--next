fb6f06144675d75cd518b3ddc43398c75cc2330e
Davud Čarovac <carovacdavud6@gmail.com>
2025-11-30 11:00:43 +0100

    checkout page, ready for paddle integration

---
+++ client/src/app/subscribe/checkout/page.tsx
@@ -0,0 +1,8 @@
+import CheckoutClient from "@/components/CheckoutPage/CheckoutClient";
+import React from "react";
+
+const page = () => {
+  return <CheckoutClient />;
+};
+
+export default page;
---
+++ client/src/components/CheckoutPage/CheckoutClient.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CheckoutClient = () => {
+  return <div>CheckoutClient</div>;
+};
+
+export default CheckoutClient;
