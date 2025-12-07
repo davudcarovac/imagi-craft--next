c8435805b0bbf74f166894188cf4ff9291374fc5
davudcarovac <carovacdavud6@gmail.com>
2025-12-07 14:39:34 +0100

    15.3.6 next version update

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,16 +1,33 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: `
+      frame-ancestors 'self' http://localhost:* https://sandbox-buy.paddle.com https://*.paddle.com;
+    `.replace(/\s{2,}/g, " "),
+  },
+];
+
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // ✅ dozvoljen domen za slike
+    domains: ["res.cloudinary.com"],
   },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      canvas: false, // 🔧 spreči webpack da traži 'canvas' modul
+      canvas: false,
     };
-
     return config;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
